63fa7e88d0115eb7caed90c1804596154c7ba905
Einstein Xavier <einstein.xavier@gmail.com>
2024-08-12 16:34:38 -0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { Post } from "./Post";
+
+
+
+export function App() {
+  return (
+    <Post author="Teste" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit laudantium accusamus blanditiis fuga libero deserunt, sit enim sapiente dolore nisi optio incidunt repudiandae qui fugit eum exercitationem laborum aperiam architecto." />
+  )
+}
