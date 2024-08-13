ebdc16ddce46184d2091bd8f3d3d4255e21dabda
Einstein Xavier <einstein.xavier@gmail.com>
2024-08-13 15:14:38 -0300

    criacao do header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { Header } from "./components/Header";
 import { Post } from "./Post";
 
 
+import './global.css'
 
 export function App() {
   return (
-    <Post author="Teste" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit laudantium accusamus blanditiis fuga libero deserunt, sit enim sapiente dolore nisi optio incidunt repudiandae qui fugit eum exercitationem laborum aperiam architecto." />
+    <div>
+      <Header />
+      <Post author="Teste" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit laudantium accusamus blanditiis fuga libero deserunt, sit enim sapiente dolore nisi optio incidunt repudiandae qui fugit eum exercitationem laborum aperiam architecto." />
+    </div>
   )
 }
